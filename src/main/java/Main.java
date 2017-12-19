593fafcdd07b5fb2aa9841e3189965836adad2f8
Abdeali Chandanwala <abdeali@wealthtech.in>
2017-12-19 11:58:39 +0530

    initial structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,58 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 public class Main {
+
+	public static ObjectMapper mapper = new ObjectMapper();
+
 	public static void main(String[] args) {
-		Main main = new Main();
-		main.parseHtml(args[0]);
+		for (String html : args) {
+			Main main = new Main();
+			try {
+				main.parseHtml(html);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	
-	public void parseHtml(String html){
+
+	public void parseHtml(String html) throws JsonProcessingException {
 		Document document = Jsoup.parse(html);
+		// parse all the table required
+		ArrayNode tableInfo = retrieveTableInfo(document);
+
+		// get the metadata from the html
+		ObjectNode metadataObject = retrieveMetadataInfo(document);
+		tableInfo.add(metadataObject);
+		System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(tableInfo));
+	}
+
+	private ObjectNode retrieveMetadataInfo(Document document) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private ArrayNode retrieveTableInfo(Document document) {
 		Elements tables = document.getElementsByTag("table");
-		tables.forEach(tableElement -> {
-			tableElement.previousSibling();
+		tables.forEach(table -> {
+			if (Validator.isTableUsefull(table))
+				return;
+			String tableTitle = getTableTitle(document, table);
+			
 		});
+
+		return null;
+	}
+
+	private String getTableTitle(Document document, Element table) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
---
+++ src/main/java/Validator.java
@@ -0,0 +1,12 @@
+import org.jsoup.nodes.Element;
+
+public class Validator {
+
+	public static boolean isTableUsefull(Element table) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	
+
+}
